a1948a09be7fcb00eb223add374e5af28cdf32e1
zeca.rt22 <zeca.rt22@gmail.com>
2022-03-17 20:21:23 -0300

    feat: imagens box, conditions color change

--- gallery_component/src/components/Galery/index.tsx
+++ gallery_component/src/components/Galery/index.tsx
@@ -1,3 +1,159 @@
-export const Galery = () => {
-  return <>Galery</>;
+import {
+  Box,
+  Center,
+  Image,
+  Text,
+  useMediaQuery,
+  HStack,
+  VStack,
+  Link as LinkK,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import { BsPatchCheckFill } from "react-icons/bs";
+
+interface GaleriaProps {
+  image: string;
+  tittle: string;
+}
+
+export const CardGaleria = ({ image, tittle }: GaleriaProps) => {
+  const [isLargerThan913] = useMediaQuery("(min-width: 913px)");
+
+  const [titleColor, setTitleColor] = useState("primary.main1");
+
+  const [backColor, setBackColor] = useState("primary.main");
+
+  const [isSelect, setIsSelect] = useState(false);
+
+  const ChangeStateColor = () => {
+    if (titleColor === "primary.main1" && backColor === "primary.main") {
+      setTitleColor("negative.main");
+      setBackColor("gray.50");
+      setIsSelect(true);
+    } else {
+      setTitleColor("primary.main1");
+      setBackColor("primary.main");
+      setIsSelect(false);
+    }
+  };
+
+  return (
+    <>
+      {isLargerThan913 ? (
+        <Box
+          onClick={() => ChangeStateColor()}
+          border="1px"
+          borderColor="gray.100"
+          mt="40px"
+          ml="100px"
+          cursor="pointer"
+          _hover={{
+            transform: "translateY(2px)",
+            borderColor: "primary.main",
+          }}
+          bg={backColor}
+          transition="border 0.2s, ease 0s, transform 0.2s"
+          borderRadius="10px  10px 0px 0px"
+          width="330px"
+          boxShadow="lg"
+          textAlign={"center"}
+          justifyContent="column"
+        >
+          <HStack spacing="-170px" marginTop={6}>
+            <Box
+              width="270px"
+              h="300px"
+              marginBottom={6}
+              align-items="center"
+              ml="30px"
+            >
+              <Image
+                borderRadius="10px 10px 0px 0px"
+                src={image}
+                w="270px"
+                h="300px"
+                objectFit="cover"
+              />
+            </Box>
+          </HStack>
+
+          <HStack spacing="122px" mb="17px" ml="15px">
+            <Box mt="-40px">
+              <IoIosArrowDropleftCircle />
+              {isSelect ? <BsPatchCheckFill /> : <></>}
+            </Box>
+
+            <Text
+              as="span"
+              w="200px"
+              h="70px"
+              display="inline-block"
+              white-space="nowrap"
+              overflow="hidden"
+              text-overflow="ellipsis"
+              color={titleColor}
+            >
+              {tittle}
+            </Text>
+          </HStack>
+        </Box>
+      ) : (
+        <Box
+          border="1px"
+          borderColor="gray.100"
+          mt="20px"
+          cursor="pointer"
+          _hover={{
+            transform: "translateY(2px)",
+            borderColor: "primary.main",
+          }}
+          bg="primary.main"
+          transition="border 0.2s, ease 0s, transform 0.2s"
+          borderRadius="10px  10px 0px 0px"
+          width="330px"
+          boxShadow="lg"
+          textAlign={"center"}
+          justifyContent="column"
+        >
+          <HStack spacing="-170px" marginTop={6}>
+            <Box
+              width="270px"
+              h="300px"
+              marginBottom={6}
+              align-items="center"
+              ml="30px"
+            >
+              <Image
+                borderRadius="10px 10px 0px 0px"
+                src={image}
+                w="270px"
+                h="300px"
+                objectFit="cover"
+              />
+            </Box>
+          </HStack>
+
+          <HStack spacing="122px" mb="17px" ml="15px">
+            <Box mt="-40px">
+              <IoIosArrowDropleftCircle />
+            </Box>
+
+            <Text
+              as="span"
+              w="200px"
+              h="70px"
+              display="inline-block"
+              white-space="nowrap"
+              overflow="hidden"
+              text-overflow="ellipsis"
+            >
+              {tittle}
+            </Text>
+          </HStack>
+        </Box>
+      )}
+    </>
+  );
 };
--- gallery_component/src/pages/Home/index.jsx
+++ gallery_component/src/pages/Home/index.jsx
@@ -1,4 +1,17 @@
+import {
+  Box,
+  Center,
+  Image,
+  Text,
+  useMediaQuery,
+  HStack,
+  VStack,
+  Flex,
+  Link as LinkK,
+} from "@chakra-ui/react";
+
 import React, { useState, useEffect } from "react";
+import { CardGaleria } from "../../components/Galery";
 
 export const Home = () => {
   const [data, setData] = useState([]);
@@ -15,5 +28,25 @@ export const Home = () => {
 
   console.log(data);
 
-  return <>{data && data.map((item) => <div>{item.url}</div>)}</>;
+  return (
+    <>
+      <Box width="95vw">
+        <Flex
+          flexDirection="row"
+          flexWrap={"wrap"}
+          justifyContent="space-around"
+          position="relative"
+          top="70px"
+          ml="22px"
+        >
+          {data &&
+            data.map((item) => (
+              <>
+                <CardGaleria image={item.url} tittle={item.title} />
+              </>
+            ))}
+        </Flex>
+      </Box>
+    </>
+  );
 };
